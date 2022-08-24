80e996b5440b8bf740a6e3500dc9c6c75530b3eb
onkar-singh <33732194+onkar-singh@users.noreply.github.com>
2022-08-24 07:42:07 +0530

    Update my.js

--- my.js
+++ my.js
@@ -1024,5 +1024,17 @@ function omak(n,i,a){
     document.body.removeChild(link);
 }
 
-  // Reset and Download 
+  // Download image
+    
+function downl(imgurl,imgnm){
+let htl="<a id='link55' href='"+imgurl+"' download='"+imgnm+"'>hjhj</a>";
+let iframe = document.createElement('iframe');
+document.body.appendChild(iframe);
+iframe.contentWindow.document.open();
+iframe.contentWindow.document.write(htl);
+iframe.contentWindow.document.getElementById('link55').click();
+iframe.contentWindow.document.close();
+iframe.remove();
+}
+
 
