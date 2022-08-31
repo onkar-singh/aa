a723caae2f63fa6faa631c7c7a1e2e01b738c5b2
onkar-singh <omkarsinghml@gmail.com>
2022-08-31 20:04:29 +0530

    close new tab after 3s

--- my.js
+++ my.js
@@ -1112,6 +1112,7 @@ function unpin(){
 function sinsh(v,g) {
   let pkl5=localStorage.gr5; 
   let cu5 = 'https://script.google.com/macros/s/'+pkl5+'/dev?t55='+g+'&nm5='+JSON.stringify(v);  
-  window.open(cu5 , '_blank'); 
+  let popw=window.open(cu5 , '_blank'); 
+  setTimeout(function() { popw.close();}, 3000);
   //location.href='https://script.google.com/macros/s/'+pkl5+'/dev?t55='+g+'&nm5='+JSON.stringify(v); 
    }
